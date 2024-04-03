4c56c283ee96332ffdb5ccb2974c0287efa40060
rajput999 <rajput9997647419@gmail.com>
2024-04-03 21:27:22 +0530

    some chanegs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,42 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/strict-type-checked',
     'plugin:react-hooks/recommended',
     'plugin:jest-dom/recommended',
     'plugin:testing-library/react',
+    'plugin:react/recommended', // Added
+    'plugin:react/jsx-runtime', // Added
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh', 'react'], // Added 'react' plugin
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-confusing-void-expression': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unnecessary-condition': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect', // Specify React version
+    },
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css'; // Import CSS file for styling
 
 type Priority = 'p1' | 'p2' | 'p3';
 
@@ -25,26 +26,44 @@ function App() {
     setTasks([
       ...tasks,
       {
-        id: new Date().getTime(), // Not a great way to generate IDs
+        id: new Date().getTime(),
         title: taskName,
         isCompleted: false,
       },
     ]);
+    setTaskName('');
+  };
+
+  const onDeleteTask = (taskId: number) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>Tasks</h1>
-      <label htmlFor="task-input">Add Task: </label>
-      <input
-        id="task-input"
-        value={taskName}
-        onChange={(e) => setTaskName(e.target.value)}
-      />
-      <button onClick={onAddTask}>Add</button>
-      <ul>
+      <div className="label-input">
+        <label htmlFor="task-input">Add Task: </label>
+        <input
+          id="task-input"
+          className="task-input"
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+        />
+        <button className="add-button" onClick={onAddTask}>
+          Add
+        </button>
+      </div>
+      <ul className="task-list">
         {tasks.map((task) => (
-          <li key={task.id}>{task.title}</li>
+          <li key={task.id} className="task-item">
+            <span className="task-title">{task.title}</span>
+            <button
+              className="delete-button"
+              onClick={() => onDeleteTask(task.id)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
